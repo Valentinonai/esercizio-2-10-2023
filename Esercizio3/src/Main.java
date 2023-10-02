c24222ae2511f0313847a1de4b01466a1615c78f
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-10-02 13:57:04 +0200

    perimetro rettangolo

--- Esercizio3/src/Main.java
+++ Esercizio3/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+     perimetroRettangolo();
+
+    }
+    public static void perimetroRettangolo(){
+        Scanner input=new Scanner(System.in);
+        System.out.println("inserisci lunghezza primo lato rettangolo");
+        double l1=Double.parseDouble(input.nextLine());
+        System.out.println("inserisci lunghezza secondo lato rettangolo");
+        double l2=Double.parseDouble(input.nextLine());
+        System.out.println("Il perimetro del rettangolo è: "+ ((l1*2)+(l2*2)));
+        input.close();
     }
 }
